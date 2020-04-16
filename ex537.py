630828bd21431ecc6f8655ea185bcbb8e14742ea
c881 <jacov.g@gmail.com>
2020-04-16 17:47:10 +0300

    Update ex537.py

--- ex537.py
+++ ex537.py
@@ -3,7 +3,7 @@ def chocolate_maker(small, big, x):
     ''' int, int, int > bool
     We recieve Small cubes of chocolate (1 cm), Big cubes (5cm) and requested
     length in centimeters.
-    we return true if we can create the requested length with the cues we have
+    we return true if we can create the requested length with the cubes we have
     >>> chocolate_maker(3, 1, 8) 
     True
     >>> chocolate_maker(3, 1, 9) 
